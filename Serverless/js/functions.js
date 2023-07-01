e8c07fd5b3f410b72548e3912c6b2b870e2ced85
Bassam Hesham <bssam2012@gmail.com>
2023-07-01 14:21:40 +0100

    Adding adjustment

--- Serverless/js/functions.js
+++ Serverless/js/functions.js
@@ -11,7 +11,7 @@ function getData(){
 
     var location = document.getElementById("locationText").value;
 
-    api_url = 'https://api.aladhan.com/v1/timingsByAddress/' + today + '?address=' + location
+    api_url = 'https://api.aladhan.com/v1/timingsByAddress/' + today + '?address=' + location + '&adjustment=1'
 
     $.getJSON(api_url, function(data, status){
         var definedTimes = [

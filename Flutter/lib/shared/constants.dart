6b95c91a1bb6a3e9932fbafa4a1aea8f14b97984
Bassam Hesham <bssam2012@gmail.com>
2023-10-20 21:20:39 +0100

    styling prayer calendar

--- Flutter/lib/shared/constants.dart
+++ Flutter/lib/shared/constants.dart
@@ -34,3 +34,18 @@ const schools = <String, int>{
   "Shafi (Standard)":0,
   "Hanafi":1,
 };
+
+Map<int, String> hijriMonthsNames = {
+  1: "Muharram",
+  2: "Safar",
+  3: "Rabi al-Awwal",
+  4: "Rabi al-Thani",
+  5: "Jumada al-Awwal",
+  6: "Jumada al-Thani",
+  7: "Rajab",
+  8: "Shaban",
+  9: "Ramadan",
+  10: "Shawwal",
+  11: "Dhu al-Qadah",
+  12: "Dhu al-Hijjah",
+};
